785110cfd4cd5727927ee90f68807f220e6f45a5
Denis Karev <dkarev@protonmail.com>
2020-10-10 18:16:37 +0200

    parsing commands

--- sheet.c
+++ sheet.c
@@ -5,23 +5,27 @@
 
 #define STR_MAX_LEN 81920 / CHAR_BIT
 
+#define MODE_NONE -1
+#define MODE_EDIT_TABLE 0
+#define MODE_EDIT_DATA 1
+
 char *delims = " ";
 
 // array of table change commands with no args
-const char tc_noargs[3][4] = {
+const char *tc_noargs[] = {
         "irow",
         "acol"
 };
 
 // array of table change commands with 1 arg
-const char tc_1arg[3][4] = {
+const char *tc_1arg[] = {
         "drow",
         "icol",
         "dcol"
 };
 
 // array of table change commands with 2 args
-const char tc_2arg[2][5] = {
+char *tc_2arg[] = {
         "drows",
         "dcols"
 };
@@ -34,7 +38,11 @@ int main(int argc, char *argv[]) {
 
     char inputBuffer[STR_MAX_LEN];
 
-    // parse arguments
+    // parsing arguments
+    // modes are edit table and edit data.
+    int mode = MODE_NONE;
+    char commands[255] = {0}; // sequence of commands (separated with ;)
+
     for (int i = 1; i < argc; ++i) {
         // parse delimiters
         if (strcmp(argv[i], "-d") == 0) {
@@ -45,14 +53,74 @@ int main(int argc, char *argv[]) {
             delims = argv[i + 1];
         }
 
-        // arguments for changing table
+        // commands for editing table
+        // commands with 2 args
+        for (int j = 0; j < (int) (sizeof(tc_2arg) / sizeof(tc_2arg[0])); ++j) {
+            if (strcmp(argv[i], tc_2arg[j]) == 0) {
+                if (i + 1 >= argc || i + 2 >= argc) {
+                    printf("ERROR: incorrect amount of arguments for the command %s", argv[i]);
+                    return EXIT_FAILURE;
+                }
 
+                // we control if arguments for the command are correct numbers
+                char *eptr;
+                long arg0 = strtol(argv[i + 1], &eptr, 10);
+                if (arg0 == 0) {
+                    printf("ERROR: incorrect argument #1: %s for the command %s", argv[i + 1], argv[i]);
+                    return EXIT_FAILURE;
+                }
+                long arg1 = strtol(argv[i + 2], &eptr, 10);
+                if (arg1 == 0) {
+                    printf("ERROR: incorrect argument #2: %s for the command %s", argv[i + 2], argv[i]);
+                    return EXIT_FAILURE;
+                }
+
+                // add command to the sequence
+                sprintf(commands, "%s%s %s %s;", commands, argv[i], argv[i + 1], argv[i + 2]);
+                mode = MODE_EDIT_TABLE;
+            }
+        }
+
+        // commands with 1 arg
+        for (int j = 0; j < (int) (sizeof(tc_1arg) / sizeof(tc_1arg[0])); ++j) {
+            if (strcmp(argv[i], tc_1arg[j]) == 0) {
+                if (i + 1 >= argc) {
+                    printf("ERROR: incorrect amount of arguments for the command '%s'", argv[i]);
+                    return EXIT_FAILURE;
+                }
+
+                // we control if arguments for the command are correct numbers
+                char *eptr;
+                long arg0 = strtol(argv[i + 1], &eptr, 10);
+                if (arg0 == 0) {
+                    printf("ERROR: incorrect argument #1: %s for the command '%s'", argv[i + 1], argv[i]);
+                    return EXIT_FAILURE;
+                }
+
+                // add command to the sequence
+                sprintf(commands, "%s%s %s;", commands, argv[i], argv[i + 1]);
+                mode = MODE_EDIT_TABLE;
+            }
+        }
+
+        // commands without args
+        for (int j = 0; j < (int) (sizeof(tc_noargs) / sizeof(tc_noargs[0])); ++j) {
+            if (strcmp(argv[i], tc_noargs[j]) == 0) {
+                // add command to the sequence
+                sprintf(commands, "%s%s;", commands, argv[i]);
+                mode = MODE_EDIT_TABLE;
+            }
+        }
+    }
+
+    if (mode == MODE_NONE) {
+        printf("ERROR: no commands were specified");
+        return EXIT_FAILURE;
     }
 
     int lastLineNumber = 0;
     while (fgets(inputBuffer, sizeof(inputBuffer), stdin)) {
-        printf("%s", inputBuffer);
-
+        //printf("%s", inputBuffer);
     }
 
     return 0;
